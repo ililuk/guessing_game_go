53f2a48bacc86926635cb7a4db4b4ac7a2213c2a
Illia Lukashuk <ilu101207@gmail.com>
2020-05-25 09:06:39 +0300

    changed game.goto show random number

--- game.go
+++ game.go
@@ -2,10 +2,12 @@ package main
 
 import(
 	"math/rand"
-         "fmt"
+	"fmt"
+	"time"
 )
 
 func main() {
+	rand.Seed(time.Now().UnixNano())
 	var numberToGuess int
 	numberToGuess = rand.Intn(100)
 	fmt.Println(numberToGuess)
