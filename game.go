e1d003fc49b0cd276d5aba43f644a3fde4847d8c
Illia Lukashuk <ilu101207@gmail.com>
2020-05-27 09:22:06 +0300

    Added user input of string value

--- game.go
+++ game.go
@@ -4,11 +4,17 @@ import(
 	"math/rand"
 	"fmt"
 	"time"
+	"bufio"
+	"os"
 )
 
 func main() {
 	rand.Seed(time.Now().UnixNano())
 	var numberToGuess int
 	numberToGuess = rand.Intn(100)
-	fmt.Println(numberToGuess)
+	var inputString string
+	fmt.Println("input number I guessed")
+        inputString , _ = bufio.NewReader(os.Stdin).ReadString('\n')
+        fmt.Println(numberToGuess)  
+        fmt.Println(inputString)
 }
