367a42262736ebd32ac84486d7f35b9bb6215cc2
Illia Lukashuk <ilu101207@gmail.com>
2020-06-02 09:07:17 +0300

    Finished game application

--- game.go
+++ game.go
@@ -6,15 +6,30 @@ import(
 	"time"
 	"bufio"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
 	rand.Seed(time.Now().UnixNano())
 	var numberToGuess int
-	numberToGuess = rand.Intn(100)
+	numberToGuess = rand.Intn(101)
 	var inputString string
-	fmt.Println("input number I guessed")
-        inputString , _ = bufio.NewReader(os.Stdin).ReadString('\n')
-        fmt.Println(numberToGuess)  
-        fmt.Println(inputString)
+	var inputNumber int
+	var tries int
+	for {
+		tries = tries + 1
+		fmt.Println("input number I guessed")
+		inputString, _ = bufio.NewReader(os.Stdin).ReadString('\n')
+		inputNumber, _ = strconv.Atoi(strings.TrimSpace(inputString))
+		if (inputNumber > numberToGuess) {
+			fmt.Println("Number I guessed is smaller. Try again")
+		} else if (inputNumber < numberToGuess) {
+			fmt.Println("Number I guessed is larger. Try again")
+		} else {
+			fmt.Println("You made", tries, "tries")
+			fmt.Println("Well done 🎉🎉🎉")
+			break
+		}		
+	}
 }
